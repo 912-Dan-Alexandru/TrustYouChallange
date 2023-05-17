40fb55db446d662662b95562b87620002433ebfd
Dan Alexandru <72348740+912-Dan-Alexandru@users.noreply.github.com>
2023-05-17 09:14:15 +0300

    async functionality

--- async_main.py
+++ async_main.py
@@ -4,6 +4,10 @@ import pandas as pd
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import csv
+import asyncio
+import aiohttp
+
+# session = aiohttp.ClientSession()
 
 class Book:
     def __init__(self, id, title, categories, authors, description, price):
@@ -23,25 +27,27 @@ class Book:
                 'Description': self.description
                 }
 
-def fetch_url(url, params=None):
+async def fetch_url(url, params=None):
     try:
-        response = requests.get(url, params=params)
-        response.raise_for_status()
-        return response.json()
-    except RequestException as e:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url, params=params) as response:
+                if response.status >= 400:
+                    raise aiohttp.ClientConnectionError(f'Connection error with status: {response.status}')
+                return await response.json()
+    except aiohttp.ClientConnectionError as e:
         print(f'Error: {e}')
     except Exception as e:
         print(f'Unexpected error: {e}')
 
     return None
 
-def get_books_by_category(category, limit=1000):
+async def get_books_by_category(category, limit=1000):
     url = f'https://openlibrary.org/subjects/{category}.json'
     params = {'category': category, 'limit': limit, 'offset': 0}
     books = []
 
     while True:
-        data = fetch_url(url, params)
+        data = await fetch_url(url, params)
         if data:
             books.extend(data.get('works', []))
         if not data or len(data.get('works', [])) < limit:
@@ -55,7 +61,7 @@ def get_categories(data):
     return []
 
 
-def get_authors(data):
+async def get_authors(data):
     authors = []
     if not isinstance(data, list):
         return authors
@@ -63,7 +69,7 @@ def get_authors(data):
         if not item.get('author') or not item.get('author').get('key'):
             continue
         auth_id = item.get('author').get('key').split("/")[2]
-        author_info = fetch_url(f'https://openlibrary.org/authors/{auth_id}.json')
+        author_info = await fetch_url(f'https://openlibrary.org/authors/{auth_id}.json')
         if author_info and author_info.get('name'):
             authors.append(author_info.get('name'))
     return authors
@@ -75,14 +81,14 @@ def get_description(data):
         return data.get('excerpts')[0].get('excerpt').get('value')
     return None
 
-def get_book_info(id):
+async def get_book_info(id):
     url = f'https://openlibrary.org/works/{id}.json'
-    data = fetch_url(url)
+    data = await fetch_url(url)
     title, categories, authors, description = None, [], [], None
     if isinstance(data, dict):
         title = data.get('title')
         categories = get_categories(data.get('subjects')) if data.get('subjects') else []
-        authors = get_authors(data.get('authors')) if data.get('authors') else []
+        authors = await get_authors(data.get('authors')) if data.get('authors') else []
         description = get_description(data)
     return title, categories, authors, description
 
@@ -92,38 +98,48 @@ def get_book_price(id, df):
             return row['Price']
     return None
 
-def get_books_objects(categories):
+async def get_book_objects_by_category(category):
     df = pd.read_csv('book_price.csv')
     book_objects = []
-    for category in categories:
-        books = get_books_by_category(category)
-        print(f'fetching books by category {category}')
-        count = 0
-        thresh = 10
-        for book in books:
-            if count*100//len(books) >= thresh:
-                print(f'Progress: {thresh}%')
-                thresh += 10 * ((count*100//len(books) - thresh)//10 + 1)
-            book_id = book["key"].split("/")[2]
-            title, categories, authors, description = get_book_info(book_id)
-            price = get_book_price(book_id, df)
-            book_objects.append(Book(book_id, title, categories, authors, description, price))
-            count += 1
+    books = await get_books_by_category(category)
+    print(f'fetching books by category {category}')
+    count = 0
+    thresh = 10
+    for book in books:
+        if count * 100 // len(books) >= thresh:
+            print(f'Progress {category}: {thresh}%')
+            thresh += 10 * ((count * 100 // len(books) - thresh) // 10 + 1)
+        book_id = book["key"].split("/")[2]
+        title, categories, authors, description = await get_book_info(book_id)
+        price = get_book_price(book_id, df)
+        book_objects.append(Book(book_id, title, categories, authors, description, price))
+        count += 1
+    print(f'finished fetching books for {category}')
     return book_objects
 
-# def generate_csv(books, filename):
-#     data = []
-#     for book in books:
-#         data.append(book.__dict__())
-#     df = pd.DataFrame(data)
-#     df.to_csv(filename, index=False)
-#
-# def generate_excel(books, filename):
-#     data = []
-#     for book in books:
-#         data.append(book.__dict__())
-#     df = pd.DataFrame(data)
-#     df.to_excel(filename, index=False, engine='openpyxl')
+async def get_book_objects_async(categories):
+    coroutines = [get_book_objects_by_category(cat) for cat in categories]
+    return await asyncio.gather(*coroutines)
+
+# async def get_books_objects(categories):
+#     df = pd.read_csv('book_price.csv')
+#     book_objects = []
+#     for category in categories:
+#         books = await get_books_by_category(category)
+#         print(f'fetching books by category {category}')
+#         count = 0
+#         thresh = 10
+#         for book in books:
+#             if count*100//len(books) >= thresh:
+#                 print(f'Progress: {thresh}%')
+#                 thresh += 10 * ((count*100//len(books) - thresh)//10 + 1)
+#             book_id = book["key"].split("/")[2]
+#             title, categories, authors, description = await get_book_info(book_id)
+#             price = get_book_price(book_id, df)
+#             book_objects.append(Book(book_id, title, categories, authors, description, price))
+#             count += 1
+#     return book_objects
+
 
 def generate_exports(books, csv_filename, excel_filename):
     data = []
@@ -147,10 +163,13 @@ def load_to_spreadsheets(filename):
     worksheet.update(data)
     spreadsheet.share('danalex1610@gmail.com', perm_type='user', role='writer')
 
-if __name__ == "__main__":
-    categories = ['python', 'database_software', 'relational_databases']
-    books = get_books_objects(categories)
-    # generate_csv(books, 'books.csv')
-    # generate_excel(books, 'books.xlsx')
-    generate_exports(books, 'books.csv', 'books.xlsx')
-    load_to_spreadsheets('books.xlsx')
+
+loop = asyncio.new_event_loop()
+asyncio.set_event_loop(loop)
+categories = ['python', 'database_software', 'relational_databases']
+books = []
+for result in loop.run_until_complete(get_book_objects_async(categories)):
+    books.extend(result)
+# books = loop.run_until_complete(get_books_objects(categories))
+generate_exports(books, 'books2.csv', 'books2.xlsx')
+load_to_spreadsheets('books2.xlsx')
